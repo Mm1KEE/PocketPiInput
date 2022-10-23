7a210579b6710069acb98cc4759eb078815fe4d2
Mm1KEE <MIKE.CHEN53@GMAIL.COM>
2022-10-23 18:51:27 +0800

    update

--- PocketPiAll/PocketPiAll.ino
+++ PocketPiAll/PocketPiAll.ino
@@ -1,12 +1,12 @@
 /*
- * @Author: Mm1KEE
- * @GitHub: https://github.com/Mm1KEE/
- * @Date: 2022-09-18 00:57:54
- * @LastEditTime: 2022-10-23 02:04:47
- * @Description: 
- * 
- * Copyright (c) 2022 by Mm1KEE, All Rights Reserved. 
- */
+   @Author: Mm1KEE
+   @GitHub: https://github.com/Mm1KEE/PocketPiInput
+   @Date: 2022-09-18 00:57:54
+ * @LastEditTime: 2022-10-23 18:34:03
+   @Description: PocketPi驱动，包含键盘、触摸、大写锁定灯、电量指示灯.详情见上方网址的READDME.
+
+   Copyright (c) 2022 by Mm1KEE, All Rights Reserved.
+*/
 #include <Keyboard.h>
 #include <Adafruit_NeoPixel.h>
 #include <SPI.h>
@@ -20,42 +20,38 @@ SmoothTouch st(touch, 2);
 void setup() {
   Keyboard.begin();
   Serial.begin(115200);
+  pixels.begin();
+  pixels.clear();
   st.setDebugInfo(0);
   st.setResolution(800, 400);
   st.begin(1, 0);
-  //while (!Serial);
+  //while (!Serial && millis()<1000);
   initKeyboard();
 }
 
 void loop() {
-
-  //updateRGB();
-  //if (!st.isTouched())updateKeyboard();
-  //Serial.println("keyboard:"+String(millis()-t));
-  //t=millis();
-  //for (int i = 0; i <= 27; i++) {
-  //}
-  //Serial.println("touch:"+String(millis()-t));
-  while (1) {
-    if (!st.update()) {
-      if(untouchedT++>15) break;
-    }
-    else untouchedT = 0;
-    //Serial.println("poll touch " + String(untouchedT));
+  for (int i = 0; i <= 9; i++) {
+    st.update();
   }
-  //Serial.println("scan keyboard");
   updateKeyboard();
-  //if (pinIndex++ >= 70) pinIndex = 0;
-  //if(pinIndex==49)Serial.println("touch:"+String(millis()-t));
-  /*t = millis();
-    for (int i = 0; i <= 69; i++) {
-    updateKeyboard(i);
-    }
-    Serial.println("touch:" + String(millis() - t));*/
+  updateRGB();
 }
 
-/*void updateRGB() {
-  if (caps) pixels.setPixelColor(0, 0, 0, 0);
-  else pixels.setPixelColor(0, 0, 255, 0);
+void updateRGB() {
+  pixels.setPixelColor(0, 0, 5 * capsLock, 0);
+  int v = readBattery();
+  byte r = max(0, 26 - v);
+  byte g = max(0, v - 24);
+  //Serial.println(String(v) + "," + String(r) + "," + String(g));
+  pixels.setPixelColor(1, r, g, 0);
   pixels.show();
-}*/
+}
+
+unsigned int readBattery() {
+  digitalWrite(PIN_SPI_SS, LOW);
+  SPI.transfer(0xA5);
+  uint16_t battery_voltage = SPI.transfer(0xA5);
+  SPI.endTransaction();
+  digitalWrite(PIN_SPI_SS, HIGH);
+  return battery_voltage;
+}
